eb6f72af6268ec0786e135e6431b00b034a4b3b7
sujin <lsj3369@gmail.com>
2023-07-06 12:03:39 +0900

    230706

--- w2/src/main/java/com/example/w2/todo/TodoListController.java
+++ w2/src/main/java/com/example/w2/todo/TodoListController.java
@@ -1,11 +1,15 @@
 package com.example.w2.todo;
 
+import com.example.w2.todo.dto.TodoDTO;
+import com.example.w2.todo.service.TodoService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "todoListController", urlPatterns = "/todo/list")
 public class TodoListController extends HttpServlet {
@@ -13,6 +17,10 @@ public class TodoListController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         System.out.println("/todo/list");
+
+        List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
+
+        req.setAttribute("list", dtoList);
 
         req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
     }
--- w2/src/main/webapp/WEB-INF/todo/list.jsp
+++ w2/src/main/webapp/WEB-INF/todo/list.jsp
@@ -6,12 +6,19 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
 <h1>List Page</h1>
+
+<ul>
+    <c:forEach var="dto" items="${list}">
+        <li>${dto}</li>
+    </c:forEach>
+</ul>
 
 </body>
 </html>
